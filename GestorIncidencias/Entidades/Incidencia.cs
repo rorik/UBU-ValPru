00de9dfd32bbbad0519b0bb2a257ac84f61a4254
Roderick <about@rorik.me>
2019-10-02 20:53:46 +0200

    Added GestorIncidencias

---
+++ GestorIncidencias/Entidades/Incidencia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Incidencia
+    {
+        public int IdIncidencia { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string Asunto { get; set; }
+        public string Comentario { get; set; }
+        public Centro Centro { get; set; }
+        public string Aula { get; set; }
+        public string Equipo { get; set; }
+        public bool Cerrada { get; set; }
+    }
+}
