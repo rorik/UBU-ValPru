ebb97a23b296e900c349942218c654fb8f24f448
Roderick <about@rorik.me>
2019-10-07 19:15:30 +0200

    Added Entity Framework

--- GestorIncidencias/Entidades/Centro.cs
+++ GestorIncidencias/Entidades/Centro.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entidades
 {
     public class Centro
     {
+        [Key]
         public string IdCentro { get; set; }
         public List<string> Aulas { get; set; }
         public string ClaveUsuario { get; set; }
