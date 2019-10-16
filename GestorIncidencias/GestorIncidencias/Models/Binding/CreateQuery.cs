70f5adc5306e6a7d76feecc0949a2b78e6d82377
Roderick <about@rorik.me>
2019-10-16 21:11:18 +0200

    Added CreateQuery

---
+++ GestorIncidencias/GestorIncidencias/Models/Binding/CreateQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestorIncidencias.Models.Binding
+{
+    public class CreateQuery
+    {
+        public string Aula { get; set; }
+        public string Asunto { get; set; }
+        public string Comentario { get; set; }
+        public string Equipo { get; set; }
+    }
+}
