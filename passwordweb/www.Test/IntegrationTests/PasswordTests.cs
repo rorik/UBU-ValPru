fb05ee7cc51948936a783afe788bc0d55612cc57
Roderick <about@rorik.me>
2019-12-14 16:51:51 +0100

    Added basic website integration tests #2

---
+++ passwordweb/www.Test/IntegrationTests/PasswordTests.cs
@@ -0,0 +1,47 @@
+﻿namespace www.Test.IntegrationTests
+{
+    public class PasswordTests
+    {
+
+        private const string BaseURL = "http://localhost:63119/";
+        private IWebDriver Driver { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver()
+            {
+                Url = BaseURL
+            };
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            try
+            {
+                Driver.Quit();
+            }
+            catch { }
+        }
+
+        [Test]
+        public void AccessWeb()
+        {
+            Assert.That(() => Driver.FindElement(By.Id("change-pwd-btn")), Throws.Nothing);
+            Driver.FindElement(By.Id("change-pwd-btn")).Click();
+            Assert.That(() => Driver.FindElement(By.Id("password-change-page")), Throws.Nothing);
+        }
+
+        [Test]
+        public void ChangePassword()
+        {
+            Driver.FindElement(By.Id("change-pwd-btn")).Click();
+            Assert.That(() => Driver.FindElement(By.Id("ctxUser")), Throws.Nothing);
+            Assert.That(() => Driver.FindElement(By.Id("ctxPassword")), Throws.Nothing);
+            Assert.That(() => Driver.FindElement(By.Id("ctxNewPassword")), Throws.Nothing);
+            Assert.That(() => Driver.FindElement(By.Id("ctxReNewPassword")), Throws.Nothing);
+            Assert.That(() => Driver.FindElement(By.Id("ctxInfo")), Throws.Nothing);
+        }
+    }
+}
--- passwordweb/www.Test/UnitTests/UsuarioTests.cs
+++ passwordweb/www.Test/UnitTests/UsuarioTests.cs
@@ -1,7 +1,4 @@
-﻿using NUnit.Framework;
-using System.Text;
-
-namespace www.Test.UnitTests
+﻿namespace www.Test.UnitTests
 {
     public class UsuarioTests
     {
