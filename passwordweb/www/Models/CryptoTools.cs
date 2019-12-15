8f3227e57586a7b13447e5836ece907a6ae3eb28
SSR1002 <ssr1002@alu.ubu.es>
2019-12-15 19:06:54 +0100

    Password encryption #16

---
+++ passwordweb/www/Models/CryptoTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace www.Models
+{
+    public class CryptoTools
+    {
+        public static string GenerateSalt()
+        {
+            //Generate a cryptographic random number.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[16];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+
+        public static string GenerateHash(string input, string salt)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input + salt);
+            SHA256Managed sHA256ManagedString = new SHA256Managed();
+            byte[] hash = sHA256ManagedString.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+        public static bool ValidateHash(string plainTextInput, string hashedInput, string salt)
+        {
+            string newHashedPin = GenerateHash(plainTextInput, salt);
+            return newHashedPin.Equals(hashedInput);
+        }
+    }
+}
